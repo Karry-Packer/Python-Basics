eb62179c89b0388c1e2cab7f3f85b63e8f781ce1
Akash kumar <68233557+Karry-Packer@users.noreply.github.com>
2026-01-24 22:21:53 +0530

    Delete CreatingDict.py

--- CreatingDict.py
+++
@@ -1,9 +0,0 @@
-People = {
-    "Guy1": {"Name": "Akash", "Age": 20, "City": "Delhi", "Country": "India"},
-    "Guy2": {"Name": "Ravi", "Age": 22, "City": "Mumbai", "Country": "India"}
-}
-
-
-
-for key, value in People.items():
-    print(f"{key}: {value}")
