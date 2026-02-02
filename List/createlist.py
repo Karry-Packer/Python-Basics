c5510bea669054a4176c60e9b7f21f1623b399dc
Akash kumar <68233557+Karry-Packer@users.noreply.github.com>
2026-02-02 23:59:40 +0530

    Add files via upload

---
+++ List/createlist.py
@@ -0,0 +1,8 @@
+lst = ["Banana", "Apple", "Orange",3,4,5.5,True
+       ,["Nested1","Nested2"],{"key1":"value1"}
+       ]
+
+print(lst)
+
+for index, value in enumerate(lst):
+    print(f"Index: {index}, Value: {value}")
