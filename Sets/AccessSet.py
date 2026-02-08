73c6142f27f2464fd8f56610160ae830417c5797
Akash kumar <68233557+Karry-Packer@users.noreply.github.com>
2026-02-08 22:27:45 +0530

    Add files via upload

---
+++ Sets/AccessSet.py
@@ -0,0 +1,7 @@
+cars = ["Ford", "Volvo", "BMW"]
+for car in cars:
+    print(car)  
+
+print("BMW" in cars)  # Check if "BMW" is in the list
+
+print("Ford" not in cars)  # Check if "Ford" is not present the list
